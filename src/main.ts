4cc6cec41304c2630624190fc66c54e7b03c74a5
sarathsuryas <sarathsuryasss48@gmail.com>
2025-03-12 20:39:32 +0530

    ready

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ async function bootstrap() {
   app.enableCors({
     origin: 'https://wrtc-angular.vercel.app', // Allow requests from any origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
-    allowedHeaders: 'Content-Type, Accept', // Allowed headers
   });
   await app.listen(process.env.PORT ?? 3000);
 }
