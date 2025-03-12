d40ff7ac8ccd249b15ac6eed2592144dec74ac37
sarathsuryas <sarathsuryasss48@gmail.com>
2025-03-12 19:39:44 +0530

    updated cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*', // Allow requests from any origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
+    allowedHeaders: 'Content-Type, Accept', // Allowed headers
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
